bd0d6b64b11fccc5055dce86d164140e25e2f919
Kayvan You <kayvan_y@yahoo.com>
2019-11-27 09:43:49 +0100

    Updates

---
+++ precache-manifest.166c8dd655b8a34ac7fde5b78b1b89f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f297e9edfc0e2d4c4c0f26608225cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61125c77a9934e73434c",
+    "url": "/static/css/main.df710eb2.chunk.css"
+  },
+  {
+    "revision": "216f272d9cf8ce7b68ad",
+    "url": "/static/js/2.1ba97002.chunk.js"
+  },
+  {
+    "revision": "61125c77a9934e73434c",
+    "url": "/static/js/main.69e936df.chunk.js"
+  },
+  {
+    "revision": "37337d9911178e31b409",
+    "url": "/static/js/runtime-main.116885be.js"
+  }
+]);
